b54ad9df492a1cf2ef38b194f3d846d83e53a546
Eric Driussi <ericdriussi@gmail.com>
2022-04-10 17:41:19 +0100

    refactor?

--- src/application/domain/FizzBuzzAllocator.ts
+++ src/application/domain/FizzBuzzAllocator.ts
@@ -4,42 +4,35 @@ import { Buzz } from './model/Buzz';
 import { Fizzbuzz } from './model/Fizzbuzz';
 
 export class FizzBuzzAllocator {
-	public static allocate (numberArray: number[]) {
-		const allocatedArray = new FizzBuzzArray();
+	public static allocate (numberArray: number[]): FizzBuzzArray {
 		const fizz = new Fizz();
 		const buzz = new Buzz();
 		const fizzBuzz = new Fizzbuzz();
 
-		for (const item of numberArray) {
-			allocatedArray.push(fizz.evaluate(buzz.evaluate(fizzBuzz.evaluate(item))));
-		}
+		const allocatedArray = numberArray.map(item => fizz.evaluate(buzz.evaluate(fizzBuzz.evaluate(item))));
 		return allocatedArray;
 	}
 
-	public static fizzArray (numberArray: number[]) {
-		const fizzArray = new FizzBuzzArray();
+	public static fizzArray (numberArray: number[]): FizzBuzzArray {
 		const fizz = new Fizz();
 
-		for (const item of numberArray) {
-			fizzArray.push(fizz.evaluate(item));
-		}
-		return fizzArray;
+		return this.parse(numberArray, fizz);
 	}
 
-	public static buzzArray (numberArray: number[]) {
-		const buzzArray = new FizzBuzzArray();
+	public static buzzArray (numberArray: number[]): FizzBuzzArray {
 		const buzz = new Buzz();
 
-		for (const item of numberArray) {
-			buzzArray.push(buzz.evaluate(item));
-		}
-		return buzzArray;
+		return this.parse(numberArray, buzz);
 	}
 
-	public static fizzBuzzArray (numberArray: number[]) {
-		const fizzBuzzArray = new FizzBuzzArray();
+	public static fizzBuzzArray (numberArray: number[]): FizzBuzzArray {
 		const fizzBuzz = new Fizzbuzz();
 
+		return this.parse(numberArray, fizzBuzz);
+	}
+
+	private static parse (numberArray: number[], fizzBuzz: Fizzbuzz) {
+		const fizzBuzzArray = new FizzBuzzArray();
 		for (const item of numberArray) {
 			fizzBuzzArray.push(fizzBuzz.evaluate(item));
 		}
